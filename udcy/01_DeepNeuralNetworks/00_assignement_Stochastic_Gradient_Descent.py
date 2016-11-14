10322b20ac6f4df78a646dc4617f53ad0f727ee4
jerrychen <jerrychen040@gmail.com>
2016-11-14 10:17:17 +0800

    finish the SGD with 1 hidden layer neural network

--- udcy/01_DeepNeuralNetworks/00_assignement_Stochastic_Gradient_Descent.py
+++ udcy/01_DeepNeuralNetworks/00_assignement_Stochastic_Gradient_Descent.py
@@ -267,8 +267,7 @@ def stochastic_gradient_descent_tensorflow(train_dataset,train_labels,valid_data
         if (step % 500 == 0):
           print("Minibatch loss at step %d: %f" % (step, l))
           print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
-          print("Validation accuracy: %.1f%%" % accuracy(
-            valid_prediction.eval(), valid_labels))
+          print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels))
           '''
           Minibatch loss at step 0: 15.809065
           Minibatch accuracy: 9.4%
@@ -298,6 +297,104 @@ def stochastic_gradient_descent_tensorflow(train_dataset,train_labels,valid_data
       ''' Test accuracy: 85.9% '''
 
   return 0
+
+def logistic_regression_SGD_1_hidden_layer(train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels):
+
+    batch_size = 128
+    num_hidden_nodes = 1024
+
+    graph = tf.Graph()
+    with graph.as_default():
+
+      # Input data. For the training data, we use a placeholder that will be fed
+      # at run time with a training minibatch.
+      tf_train_dataset = tf.placeholder(tf.float32,
+                                        shape=(batch_size, image_size * image_size))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_valid_dataset = tf.constant(valid_dataset)
+      tf_test_dataset = tf.constant(test_dataset)
+
+      # Variables.
+      weights1 = tf.Variable(
+        tf.truncated_normal([image_size * image_size, num_hidden_nodes]))
+      biases1 = tf.Variable(tf.zeros([num_hidden_nodes]))
+      weights2 = tf.Variable(
+        tf.truncated_normal([num_hidden_nodes, num_labels]))
+      biases2 = tf.Variable(tf.zeros([num_labels]))
+
+      # Training computation.
+      lay1_train = tf.nn.relu(tf.matmul(tf_train_dataset, weights1) + biases1)
+      logits = tf.matmul(lay1_train, weights2) + biases2
+      loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+
+      # Optimizer.
+      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+      # Predictions for the training, validation, and test data.
+      train_prediction = tf.nn.softmax(logits)
+      lay1_valid = tf.nn.relu(tf.matmul(tf_valid_dataset, weights1) + biases1)
+      valid_prediction = tf.nn.softmax(tf.matmul(lay1_valid, weights2) + biases2)
+      lay1_test = tf.nn.relu(tf.matmul(tf_test_dataset, weights1) + biases1)
+      test_prediction = tf.nn.softmax(tf.matmul(lay1_test, weights2) + biases2)
+
+
+
+
+    num_steps = 3001
+
+    with tf.Session(graph=graph) as session:
+      tf.initialize_all_variables().run()
+      print("Initialized")
+      for step in range(num_steps):
+        # Pick an offset within the training data, which has been randomized.
+        # Note: we could use better randomization across epochs.
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        # Generate a minibatch.
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        # Prepare a dictionary telling the session where to feed the minibatch.
+        # The key of the dictionary is the placeholder node of the graph to be fed,
+        # and the value is the numpy array to feed to it.
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run(
+          [optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 500 == 0):
+          print("Minibatch loss at step %d: %f" % (step, l))
+          print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+          print("Validation accuracy: %.1f%%" % accuracy(
+            valid_prediction.eval(), valid_labels))
+          '''
+          Minibatch loss at step 0: 352.830933
+          Minibatch accuracy: 7.0%
+          Validation accuracy: 23.2%
+          Minibatch loss at step 500: 27.305231
+          Minibatch accuracy: 80.5%
+          Validation accuracy: 78.3%
+          Minibatch loss at step 1000: 12.617765
+          Minibatch accuracy: 79.7%
+          Validation accuracy: 80.9%
+          Minibatch loss at step 1500: 5.883183
+          Minibatch accuracy: 89.8%
+          Validation accuracy: 80.2%
+          Minibatch loss at step 2000: 3.081453
+          Minibatch accuracy: 87.5%
+          Validation accuracy: 81.1%
+          Minibatch loss at step 2500: 2.968213
+          Minibatch accuracy: 81.2%
+          Validation accuracy: 81.6%
+          Minibatch loss at step 3000: 1.669900
+          Minibatch accuracy: 83.6%
+          Validation accuracy: 81.6%
+
+          [Finished in 83.5s]
+          '''
+      print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
+      '''Test accuracy: 89.4%'''
+
+
+    return 0
+
 
 #########################################
 def main():
@@ -332,7 +429,7 @@ def main():
     # Let's load all the data into TensorFlow and
     # build the computation graph corresponding to our training:
 
-    normal_gradient_descent_tensorflow(train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels)
+    #normal_gradient_descent_tensorflow(train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels)
 
 
 
@@ -346,6 +443,8 @@ def main():
     # with rectified linear units nn.relu() and 1024 hidden nodes.
     # This model should improve your validation / test accuracy.
     ########################
+    logistic_regression_SGD_1_hidden_layer(train_dataset,train_labels,valid_dataset,valid_labels,test_dataset,test_labels)
+
     return 0
 
 main()
